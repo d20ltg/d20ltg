4fc9df204cab50873b0b48285da9517bc18d1585
Murphy <murphyjstevens@gmail.com>
2015-11-11 16:31:50 -0600

    fix all the things

--- db/migrate/20151021212745_create_announcements.rb
+++ db/migrate/20151021212745_create_announcements.rb
@@ -2,6 +2,7 @@ class CreateAnnouncements < ActiveRecord::Migration
   def change
     create_table :announcements do |t|
       t.string :image
+      t.text :content
       t.string :title
 
       t.timestamps
