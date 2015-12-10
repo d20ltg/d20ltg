1e3a3e16aae6907f31977c83e1c54dc544acc104
Brian Stewart <brian@thecodesmith.com>
2015-12-09 18:04:15 -0600

    Updated db/seeds.rb to dynamically reference block and expansion ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,36 +25,36 @@ Expansion.delete_all
 Card.delete_all
 TabletopGame.delete_all
 
-Block.create block_name: 'Innistrad'
-Block.create block_name: 'Khans of Tarkir'
+block1 = Block.create block_name: 'Innistrad'
+block2 = Block.create block_name: 'Khans of Tarkir'
 
-Expansion.create set_name: 'Innistrad', image: seed_image('liliana.jpg'), block_id: 1
-Expansion.create set_name: 'Dark Ascension', image: seed_image('sorin.jpg'), block_id: 1
-Expansion.create set_name: 'Avacyn Restored', image: seed_image('avacyn.jpg'), block_id: 1
+ex1 = Expansion.create set_name: 'Innistrad',       image: seed_image('liliana.jpg'), block_id: block1.id
+ex2 = Expansion.create set_name: 'Dark Ascension',  image: seed_image('sorin.jpg'),   block_id: block1.id
+ex3 = Expansion.create set_name: 'Avacyn Restored', image: seed_image('avacyn.jpg'),  block_id: block1.id
 
-Card.create name: 'Forest', card_type: 'Land', expansion_id: 1
-Card.create name: 'Forest', card_type: 'Land', expansion_id: 2
-Card.create name: 'Forest', card_type: 'Land', expansion_id: 3
-Card.create name: 'Island', card_type: 'Land', expansion_id: 1
-Card.create name: 'Island', card_type: 'Land', expansion_id: 2
-Card.create name: 'Island', card_type: 'Land', expansion_id: 3
-Card.create name: 'Liliana of the Veil', card_type: 'Planeswalker - Liliana', expansion_id: 1
-Card.create name: 'Doomsayer', card_type: 'Creature - Human', expansion_id: 1
-Card.create name: 'Thalia, Guardian of Thraben', card_type: 'Legendary Creature - Human', expansion_id: 3
-Card.create name: 'Think Twice', card_type: 'Instant', expansion_id: 1
-Card.create name: 'Bonfire of the Damned', card_type: 'Sorcey', expansion_id: 3
-Card.create name: 'Entreat the Angels', card_type: 'Sorcey', expansion_id: 3
-Card.create name: 'Sorin Lord of Innistrad', card_type: 'planeswalker - Sorin', expansion_id: 2
-Card.create name: 'Wolfir Silverheart', card_type: 'Creature - Wolf', expansion_id: 3
-Card.create name: 'Terminus', card_type: 'Sorcey', expansion_id: 3
-Card.create name: 'Snapcaster Mage', card_type: 'Creature - Wizard', expansion_id: 1
-Card.create name: 'Lingering Souls', card_type: 'Sorcey', expansion_id: 2
-Card.create name: 'Thraben Doomsayer', card_type: 'Creature - Human Cleric', expansion_id: 2
-Card.create name: "Geralf's Messenger", card_type: 'Creature - Zombie', expansion_id: 2
-Card.create name: 'Gravecaller', card_type: 'Creature - Zombie', expansion_id: 2
+Card.create name: 'Forest',                      card_type: 'Land',                       expansion_id: ex1.id
+Card.create name: 'Forest',                      card_type: 'Land',                       expansion_id: ex2.id
+Card.create name: 'Forest',                      card_type: 'Land',                       expansion_id: ex3.id
+Card.create name: 'Island',                      card_type: 'Land',                       expansion_id: ex1.id
+Card.create name: 'Island',                      card_type: 'Land',                       expansion_id: ex2.id
+Card.create name: 'Island',                      card_type: 'Land',                       expansion_id: ex3.id
+Card.create name: 'Liliana of the Veil',         card_type: 'Planeswalker - Liliana',     expansion_id: ex1.id
+Card.create name: 'Doomsayer',                   card_type: 'Creature - Human',           expansion_id: ex1.id
+Card.create name: 'Thalia, Guardian of Thraben', card_type: 'Legendary Creature - Human', expansion_id: ex3.id
+Card.create name: 'Think Twice',                 card_type: 'Instant',                    expansion_id: ex1.id
+Card.create name: 'Bonfire of the Damned',       card_type: 'Sorcey',                     expansion_id: ex3.id
+Card.create name: 'Entreat the Angels',          card_type: 'Sorcey',                     expansion_id: ex3.id
+Card.create name: 'Sorin Lord of Innistrad',     card_type: 'planeswalker - Sorin',       expansion_id: ex2.id
+Card.create name: 'Wolfir Silverheart',          card_type: 'Creature - Wolf',            expansion_id: ex3.id
+Card.create name: 'Terminus',                    card_type: 'Sorcey',                     expansion_id: ex3.id
+Card.create name: 'Snapcaster Mage',             card_type: 'Creature - Wizard',          expansion_id: ex1.id
+Card.create name: 'Lingering Souls',             card_type: 'Sorcey',                     expansion_id: ex2.id
+Card.create name: 'Thraben Doomsayer',           card_type: 'Creature - Human Cleric',    expansion_id: ex2.id
+Card.create name: "Geralf's Messenger",          card_type: 'Creature - Zombie',          expansion_id: ex2.id
+Card.create name: 'Gravecaller',                 card_type: 'Creature - Zombie',          expansion_id: ex2.id
 
-TabletopGame.create set_name: 'Monopoly', image: seed_image('monopoly.jpg')
-TabletopGame.create set_name: 'Settlers of Catan', image: seed_image('settlers.jpg')
+game1 = TabletopGame.create set_name: 'Monopoly',          image: seed_image('monopoly.jpg')
+game2 = TabletopGame.create set_name: 'Settlers of Catan', image: seed_image('settlers.jpg')
 
-Game.create name: 'Star Wars Monopoly', tabletop_game_id: 0
-Game.create name: 'Star Trek Settlers of Catan', tabletop_game_id: 1
+Game.create name: 'Star Wars Monopoly',          tabletop_game_id: game1.id
+Game.create name: 'Star Trek Settlers of Catan', tabletop_game_id: game2.id
